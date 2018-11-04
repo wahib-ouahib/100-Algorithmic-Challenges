d85911840d992730ade2810979397b8e5e47d3aa
wahib <wahib.med.ouahib@gmail.com>
2018-11-04 23:33:37 +0000

    #

---
+++ Longest.js
@@ -0,0 +1,27 @@
+function LongestWord(sen) { 
+    a=''
+    x=0;
+    for(var i=0;i<sen.length;i++){l
+
+    }  
+
+    return sen; 
+           
+  }
+  
+  a = 'i love code';
+  console.log(a(0));
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <script src="factorial.js">    </script>
+    
+</body>
+</html>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, voluptate officia? Ratione debitis sapiente dolorem soluta vitae quo, officiis perspiciatis rem adipisci ullam facilis magnam assumenda dolore voluptas fuga delectus eum illum harum alias laudantium velit doloremque quidem ea? Eaque tenetur, ut dolores incidunt maiores ullam aspernatur, qui vitae, labore quisquam accusamus. Inventore nemo molestias adipisci officiis animi suscipit voluptatem eos nesciunt nam quos assumenda nisi a esse autem, iusto dignissimos, cupiditate, velit atque necessitatibus. Alias quisquam maiores est, doloribus sunt inventore vitae similique rem harum consequatur eius pariatur blanditiis quia tempora quis dolores laudantium doloremque? Voluptates recusandae nobis quis?
